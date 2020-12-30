364c17dcc21f197e22314031866912917cf06813
SalavatM <techprosm@yandex.ru>
2020-12-30 22:42:30 +0300

    Lesson7. Bean scope

--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -11,11 +11,22 @@ public class TestSpring {
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
 
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
 
-        musicPlayer.playMusic();
+        boolean comparison = musicPlayer == musicPlayer2;
+        System.out.println(comparison);
 
-//        System.out.println(musicPlayer.getName());
-//        System.out.println(musicPlayer.getVolume());
+        System.out.println(musicPlayer);
+        System.out.println(musicPlayer2);
+
+        musicPlayer.setVolume(10);
+
+
+        //musicPlayer.playMusic();
+
+        //System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+        System.out.println(musicPlayer2.getVolume());
 
         context.close();
     }
